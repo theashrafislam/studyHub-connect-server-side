0217f2de5c2b261d465dd8e87d08ad8e8b89db54
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-05-12 23:46:44 +0600

    node_modules be gone!

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express())
 app.use(cors())
 
+
+console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gphdl2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const assignmentCollocation = client.db("studyHub-connect").collection("assignment")
+
+
+        app.post('/create-assignment', async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            res.send(data)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -15,4 +52,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`I am running on port: ${port}`)
-  })
+})
