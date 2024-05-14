7cedc0126a7ea6a03a7cba2bb726aa05a05a33ec
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-05-14 09:15:57 +0600

    submitted-assignment api is complate

--- index.js
+++ index.js
@@ -30,28 +30,96 @@ async function run() {
         // await client.connect();
 
         const assignemntCollection = client.db('studyHub').collection('assignment')
+        const submittedAssignmentCollection = client.db('studyHub').collection('submittedAssignment')
 
-    app.post("/create-assignment", async(req, res) => {
+        app.post("/create-assignment", async (req, res) => {
             const assignemt = req.body;
             const result = await assignemntCollection.insertOne(assignemt);
             res.send(result)
         })
 
-    app.get("/all-assignment", async(req, res) => {
-        const result = await assignemntCollection.find().toArray();
-        res.send(result)
-    })
+        app.get("/all-assignment", async (req, res) => {
+            const result = await assignemntCollection.find().toArray();
+            res.send(result)
+        })
 
-    app.delete("/all-assignment/:id", async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await assignemntCollection.deleteOne(query);
-        res.send(result)
-    })
+        app.delete("/all-assignment/:id", async (req, res) => {
+            const id = req.params.id;
+            const userEmail = req.body.userEmail;
+
+            const query = { _id: new ObjectId(id) }
+            const assignment = await assignemntCollection.findOne(query);
+            console.log(assignment?.userEmail);
+
+            if (!assignment) {
+                return res.status(404).send({ error: "Assignment not found" });
+            }
+            else if (userEmail !== assignment.userEmail) {
+                return res.status(403).send({ error: "Unauthorized: You do not have permission to delete this assignment" });
+            }
+
+            const result = await assignemntCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.get("/all-assignment/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await assignemntCollection.findOne(query);
+            res.send(result)
+        })
+        app.put("/all-assignment/:id", async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    title: data.title,
+                    marks: data.marks,
+                    description: data.description,
+                    image: data.image,
+                    difficulty: data.difficulty,
+                    date: data.date
+                },
+            };
+            const result = await assignemntCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+        app.post("/submitted-assignment", async (req, res) => {
+            const { userEmail, id, pdfDocLink, additionalNotes, title, displayName } = req.body;
+            const submissionStatus = "Pending";
+            const submissionData = {
+                userEmail,
+                id,
+                pdfDocLink,
+                additionalNotes,
+                submissionStatus,
+                title,
+                displayName
+            };
+            const result = await submittedAssignmentCollection.insertOne(submissionData)
+            res.send(result)
+        })
+
+        app.get("/submitted-assignment/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email }
+            console.log(query);
+            const result = await submittedAssignmentCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get("/submitted-assignment", async(req, res) => {
+            const query = {submissionStatus: 'Pending'}
+            const result = await submittedAssignmentCollection.find(query).toArray();
+            res.send(result)
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
